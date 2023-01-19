ba0349f33e71859c0c634d4d105817a5346ab399
mingun choi <ryanchoi100826@gmail.com>
2023-01-18 23:15:10 -0500

    done

--- client.js
+++ client.js
@@ -1,32 +1,26 @@
 const net = require("net");
-const myObject = require("./play");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+  const conn = net.createConnection({ 
+    Host: '165.227.47.243',
+    Port: 50541
+  });
+
+conn.setEncoding("utf8");
+
+conn.on("data", (data)=>{
+  console.log(data)
   });
 
   conn('connect', (connect) => {
     console.log("Successfully connected to game server");
     conn.write("Name: mgc");
-  })
-
-
-  // setInterval(()=>{
-  //   conn.write("Move: up");
-  // },50)
-
-  conn.setEncoding("utf8");
-
-
+})
 
   return conn;
 };
-
-console.log("Connecting ...");
-connect();
 
 module.exports = { connect };
 
